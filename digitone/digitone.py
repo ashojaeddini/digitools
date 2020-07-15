7e5e73fd6ff33053a50ca32c96e71230ac17ed73
Ali Shojaeddini <37891382+ashojaeddini@users.noreply.github.com>
2020-07-14 23:10:02 -0400

    Adds support for updating SysEx file from a CSV file

--- digitone/digitone.py
+++ digitone/digitone.py
@@ -44,81 +44,127 @@ class Tag(enum.Enum):
 
 
 class Sound:
+    def __init__(self, pnum: int, data: bytes):
+        self._pnum = pnum
+        self._data = bytearray(data)
+        self._tags = self._read_tags()
+        self._name = self._read_name()
 
-    def __init__(self, message: bytes):
-        # TODO: Validate the message
-        # - Verify manufacturer/model ID
-        # - Verify data length based on length in EOM
-        # - Verify message integrity based on checkum in EOM
+    def _write_bytes(self, start: int, bs: bytes):
+        for i, b in enumerate(bs):
+            self._data[start + i] = b
 
-        data = Sound.decode(message[0x0A : len(message) - 5])
-
-        self._message = message
-        self._data = data
-        self._tags = Sound.extract_tags(data)
-        self._name = Sound.extract_name(data)
-
-    @staticmethod
-    def decode(data: bytes) -> bytes:
-        length = len(data)
-
-        if length % 8 == 1:
-            raise TypeError('Provided data is not encoded as expected')
-
-        result = bytearray()
-
-        # Divide data into 8-byte groups
-
-        si = 0
-
-        while si < length:
-            ei = min(si + 8, length)
-            bg = data[si : ei]
-
-            # Process the byte group
-
-            msb_byte = bg[0]
-
-            for i in range(1, len(bg)):
-                msb = (msb_byte >> (7 - i)) & 1
-                result.append(bg[i] | (msb << 7))
-
-            si = ei
-        
-        return bytes(result)
-
-    @staticmethod
-    def extract_tags(data: bytes) -> list:
-        tag_data = data[0x08 : 0x0C]
-
+    def _read_tags(self) -> list:
         tags = []
 
         for t in Tag:
-            if (tag_data[3 - (t.value // 8)] >> (t.value % 8)) & 1:
+            if self._data[11 - (t.value // 8)] >> (t.value % 8) & 1:
                 tags.append(t)
 
         return tags
 
-    @staticmethod
-    def extract_name(data: bytes) -> str:
-        name_data = data[0x0C : min(data.index(0x00, 0x0C), 0x1B)]
+    def _write_tags(self, tags: list):
+        tag_bytes = bytearray(4)
 
-        return str(name_data, 'latin-1')
+        for t in Tag:
+            if t in tags:
+                tag_bytes[3 - (t.value // 8)] |= 1 << (t.value % 8)
 
-    def message(self) -> bytes:
-        return self._message
+        self._write_bytes(8, tag_bytes)
+
+    def _read_name(self) -> str:
+        return str(self._data[12 : min(self._data.index(0x00, 12), 27)], 'latin-1')
+
+    def _write_name(self, name: str):
+        if len(name) > 15:
+            TypeError('Maximum length for sound name is 15') # TODO: Confirm that max length is 15
+
+        name_bytes = bytearray(name.encode('latin-1'))
+        name_bytes.append(0x00)
+
+        self._write_bytes(12, name_bytes)
+
+    def pnum(self) -> int:
+        return self._pnum
 
     def data(self) -> bytes:
         return self._data
 
-    def name(self) -> str:
+    def name(self, value : str = None) -> str:
+        if value:
+            self._write_name(value)
+            self._name = value
+        
         return self._name
 
-    def tags(self) -> list:
+    def tags(self, value : list = None) -> list:
+        if value:
+            self._write_tags(value)
+            self._tags = value
+        
         return self._tags
 
     def __str__(self):
-        return f'{self._name} {self._tags}'
+        return f'{self._name:15} {[t.name for t in self._tags]}'
+
+
+class SoundSysExHandler:
+    PREFIX = bytes((0x00, 0x20, 0x3C, 0x0D, 0x00, 0x53, 0x01, 0x01))
+
+    @staticmethod
+    def validate(message: bytes):
+        # Validate the message
+        # - Verify manufacturer/model ID
+        # - Verify message integrity based on checkum in EOM
+        # - Verify data length based on length in EOM
+
+        eom = message[len(message) - 5 :]
+
+        v1 = message[1 : 9] == SoundSysExHandler.PREFIX
+        v2 = SoundSysExHandler.checksum(message[10 : len(message) - 5]) == eom[0 : 2]
+        v3 = SoundSysExHandler.encode_number(len(message) - 10) == eom[2 : 4]
+
+        # TODO: Raise individual errors for each validation 
+
+        if not (v1 and v2 and v3):
+            raise TypeError('Invalid sound data encountered')
+
+    @staticmethod
+    def message_to_sound(message: bytes) -> Sound:
+        SoundSysExHandler.validate(message) # TODO: Handle error to show a message
+
+        pnum = message[9]
+        data = sysex.SysExDataEncoder.decode(message[10 : len(message) - 5])
+
+        return Sound(pnum, data)
+
+    @staticmethod
+    def sound_to_message(sound: Sound) -> bytes:
+        encoded_data = sysex.SysExDataEncoder.encode(sound.data())
+        checksum = SoundSysExHandler.checksum(encoded_data)
+        length = SoundSysExHandler.encode_number(len(encoded_data) + 5)
+
+        message = bytearray()
+        message.append(sysex.MSG_START)
+        message.extend(SoundSysExHandler.PREFIX)
+        message.append(sound.pnum())
+        message.extend(encoded_data)
+        message.extend(checksum)
+        message.extend(length)
+        message.append(sysex.MSG_END)
+
+        return bytes(message)
+    
+    @staticmethod
+    def checksum(data: bytes) -> bytes:
+        return SoundSysExHandler.encode_number(sum(data))
+
+    @staticmethod
+    def encode_number(n: int) -> bytes:
+        lsb = 0x7F & n
+        msb = 0x7F & n >> 7
+
+        return bytes((msb, lsb))
 
 
 class SoundManager:
@@ -133,14 +179,48 @@ class SoundManager:
         Returns:
             A list of Sound objects loaded from the input file
         """
-        messages = sysex.SysEx.load(syx_file)
+        messages = sysex.SysExHandler.load(syx_file)
 
         sounds = []
 
         for message in messages:
-            sounds.append(Sound(message))
+            sounds.append(SoundSysExHandler.message_to_sound(message))
 
         return sounds
+
+    @staticmethod
+    def save(sounds: list, syx_file: str):
+        """Saves the provided sounds to the specified file.
+
+        Args:
+            sounds: the list of sounds to save
+            syx_file: Path to the input SysEx file
+        """
+        messages = []
+
+        for sound in sounds:
+            messages.append(SoundSysExHandler.sound_to_message(sound))
+
+        sysex.SysExHandler.save(messages, syx_file)
+    
+    @staticmethod
+    def test(syx_file: str):
+        sounds = SoundManager.load(syx_file)
+
+        ml = []
+
+        s1 = sounds[0]
+        s1.name('Test Sound 01')
+        s1.tags((Tag.TXTR, Tag.ATMO, Tag.EVOL))
+
+        s2 = sounds[1]
+        s2.name('Test Sound 02')
+        s2.tags((Tag.PAD, Tag.SOFT, Tag.VNTG))
+
+        ml.append(SoundSysExHandler.sound_to_message(s1))
+        ml.append(SoundSysExHandler.sound_to_message(s2))
+
+        sysex.SysExHandler.save(syx_file, ml)
     
     @staticmethod
     def print(syx_file: str):
@@ -172,16 +252,39 @@ class SoundManager:
         sounds = SoundManager.load(syx_file)
 
         with open(csv_file, 'w', newline='') as f:
-            csvwriter = csv.writer(f, dialect='excel')
+            writer = csv.writer(f, dialect='excel')
             
-            # Write the header row
-            csvwriter.writerow(['#', 'Sound Name'] + [t.name for t in Tag])
+            writer.writerow(['#', 'Sound Name'] + [t.name for t in Tag])
 
-            # Write a row for each sound
             for i, sound in enumerate(sounds, start=1):
                 sound_row = [f'{i:03}', sound.name()]
 
                 for t in Tag:
                     sound_row.append('●' if t in sound.tags() else '')
                 
-                csvwriter.writerow(sound_row)
+                writer.writerow(sound_row)
+    
+    @staticmethod
+    def update(syx_file: str, csv_file: str):
+        """Updates the sounds in the SysEx file base on values in the CSV file.
+
+        Args:
+            syx_file: Path to the SysEx file to update
+            csv_file: Path to the CSV file with updates
+        """
+        sounds = SoundManager.load(syx_file)
+
+        with open(csv_file, 'r', newline='') as f:
+            reader = csv.DictReader(f, dialect='excel')
+
+            for row in reader:
+                tags = []
+                for t in Tag:
+                    if row[t.name]:
+                        tags.append(t)
+                
+                s = sounds[int(row['#']) - 1]
+                s.name(row['Sound Name'])
+                s.tags(tags)
+            
+        SoundManager.save(sounds, syx_file)
--- digitone/sysex.py
+++ digitone/sysex.py
@@ -1,34 +1,99 @@
 """A set of utility functions for loading and parsing standard SysEx files.
 """
 
-SYSEX_BEGIN = 0xF0
-SYSEX_END = 0xF7
-MESSAGE_SIZE = 361
+MSG_START = 0xF0
+MSG_END = 0xF7
 
-class SysEx:
+class SysExHandler:
 
     @staticmethod
-    def load(syx_file: str) -> bytes:
+    def load(syx_file: str) -> list:
         """Returns the individual SysEx messages in the input file as a list of bytes objects
         """
         with open(syx_file, mode='rb') as f:
             contents = f.read()
         
-        # TODO: Split into individual messages based on SysEx star/end (i.e. F0/F7) bytes instead
-
         messages = []
 
-        for i in range(len(contents) // MESSAGE_SIZE):
-            messages.append(contents[i * MESSAGE_SIZE : (i + 1) * MESSAGE_SIZE])
+        si = 0
+        while si < len(contents):
+            if contents[si] != MSG_START:
+                raise TypeError('Invalid SysEx message encountered')
+            
+            ei = contents.find(MSG_END, si + 1) + 1
+            messages.append(contents[si : ei])
+            si = ei
 
         return messages
+
+    @staticmethod
+    def save(messages: list, syx_file: str):
+        """Stores the provided SysEx messages to the specified file
+        """
+        with open(syx_file, mode='wb') as f:
+            for message in messages:
+                f.write(message)
 
     @staticmethod
     def split(syx_file: str):
         """Saves the individual SysEx messages in the input file as individual files
         """
-        messages = SysEx.load(syx_file)
+        messages = SysExHandler.load(syx_file)
 
         for i, message in enumerate(messages):
             with open(f'{i+1:03}.syx', mode='wb') as f:
                 f.write(message)
+
+
+class SysExDataEncoder:
+
+    @staticmethod
+    def encode(data: bytes) -> bytes:
+        """Performs 7-bit encoding on the data for use in SysEx messages
+        """
+        result = bytearray()
+
+        for g in SysExDataEncoder.group(data, 7):
+            e = bytearray(len(g) + 1)
+            
+            for i in range(0, len(g)):
+                e[0] = (g[i] >> 7) << (6 - i) | e[0]
+                e[i + 1] = g[i] & 0x7F
+            
+            result.extend(e)
+        
+        return bytes(result)
+
+    @staticmethod
+    def decode(data: bytes) -> bytes:
+        """Performs 7-bit decoding on the data to yield original raw data
+        """
+        if len(data) % 8 == 1:
+            raise TypeError('Provided data is not encoded as expected')
+
+        result = bytearray()
+
+        for g in SysExDataEncoder.group(data, 8):
+            for i in range(1, len(g)):
+                msb = g[0] >> (7 - i) & 1
+                result.append(g[i] | msb << 7)
+        
+        return bytes(result)
+
+    @staticmethod
+    def group(data: bytes, size: int) -> list:
+        """Splits the data into groups of specified size
+        """
+        result = []
+
+        l = len(data)
+        d = l // size
+        r = l % size
+
+        for i in range(d):
+            result.append(data[i * size : (i + 1) * size])
+
+        if r > 0:
+            result.append(data[l - r :])
+
+        return result
